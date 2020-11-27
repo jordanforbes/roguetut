67a0e72a4e4eed0840f94281a76ba1231db03e88
jordanforbes <jordforb@gmail.com>
2020-11-27 12:03:22 -0500

    fixed arena drawing

--- Program.cs
+++ Program.cs
@@ -21,15 +21,16 @@ namespace csroguetut
 
             //strings
             string Draw_Char = "X";
-            string[,] Draw_Game_Map = new string[119,39];
+            string[,] Draw_Game_Map = new string[119,29];
 
             //Program
             do {
                 Console.Clear();
-                int Spawn_Point_Generator_X =  Rand.Next (0, 119);
-                int Spawn_Point_Generator_Y =  Rand.Next (0, 39);
                 int Spawn_Point_Height = Rand.Next(5,8);
                 int Spawn_Point_Width = Rand.Next(7,10);
+                int Spawn_Point_Generator_X =  Rand.Next (0, 119 - Spawn_Point_Width-1);
+                int Spawn_Point_Generator_Y =  Rand.Next (0, 29 - Spawn_Point_Height-1);
+               
 
                 for(int y = 0; y <= Spawn_Point_Height; y++){
                     for(int x = 0; x <= Spawn_Point_Width; x++){
@@ -37,8 +38,8 @@ namespace csroguetut
                             int X_Difference = Spawn_Point_Generator_X + Spawn_Point_Width - 120;
                             Spawn_Point_Generator_X = Spawn_Point_Generator_X - X_Difference;
                         } 
-                        if(Spawn_Point_Generator_Y + Spawn_Point_Height >= 38 ){
-                            int Y_Difference = Spawn_Point_Generator_Y + Spawn_Point_Height - 41;
+                        if(Spawn_Point_Generator_Y + Spawn_Point_Height >= 28 ){
+                            int Y_Difference = Spawn_Point_Generator_Y + Spawn_Point_Height - 29;
                             Spawn_Point_Generator_Y = Spawn_Point_Generator_Y - Y_Difference;
                         }
                         if(y == 0 || y == Spawn_Point_Height){
@@ -51,13 +52,13 @@ namespace csroguetut
                         
                     }
                 }
-                for(int y_Draw = 0; y_Draw <= 38; y_Draw++){
+                for(int y_Draw = 0; y_Draw <= 28; y_Draw++){
                     for(int x_Draw = 0; x_Draw <= 118; x_Draw++){
                         Console.SetCursorPosition(x_Draw,y_Draw);
                         Console.Write(Draw_Game_Map[x_Draw, y_Draw]);
                     }
                 }
-                Console.SetCursorPosition(0,39);
+                Console.SetCursorPosition(0,29);
                 Console.Write("HEALTH: {0}/{1}", Char_Health_Current, Char_Health_Full);
                 Console.ReadLine();
             }while(Gameover == 0);
